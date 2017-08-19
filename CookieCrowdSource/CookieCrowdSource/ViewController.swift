b753f857cde5a287ef09fa81d25cd90f814f3c41
noahbragg <noahbragg@cedarville.edu>
2017-08-19 19:39:33 -0400

    added pod alamofire, added Stripe template for paying

--- CookieCrowdSource/CookieCrowdSource/ViewController.swift
+++ CookieCrowdSource/CookieCrowdSource/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let settings = SettingsViewController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func tapCheckout(_ sender: Any) {
+        let controller = CheckoutViewController(product: "Cookies", price: 1200, settings: settings.settings)
+        self.present(controller, animated: true, completion: nil)
+    }
+    
+    @IBAction func tapSettings(_ sender: Any) {
+        self.present(settings, animated: true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
