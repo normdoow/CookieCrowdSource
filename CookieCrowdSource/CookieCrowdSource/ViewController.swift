9bf9e87f98452046bc8e229fa6e3d13e68c43489
noahbragg <noahbragg@cedarville.edu>
2017-08-19 16:36:03 -0400

    created the project

---
+++ CookieCrowdSource/CookieCrowdSource/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CookieCrowdSource
+//
+//  Created by Noah Bragg on 8/19/17.
+//  Copyright © 2017 Noah Bragg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
