f4a0593806ced4f0a2f8f5a762786d26c34175ee
noahbragg <noahbragg@cedarville.edu>
2017-08-26 22:03:57 -0400

    added location tracking to make sure the user is close to the dayton ohio area. made different buttons to show the results

---
+++ CookieCrowdSource/CookieCrowdSource/LocationChecker.swift
@@ -0,0 +1,56 @@
+//
+//  LocationManager.swift
+//  CookieCrowdSource
+//
+//  Created by Noah Bragg on 8/26/17.
+//  Copyright © 2017 Noah Bragg. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationChecker : NSObject, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    var currentLocation = CLLocationCoordinate2D()
+    var coord = CLLocationCoordinate2D()
+    
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        
+        coord = CLLocationCoordinate2D()
+        coord.latitude = 39.691483              //coordinates for our apartment
+        coord.longitude = -84.101717
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        currentLocation = manager.location!.coordinate
+        print("locations = \(currentLocation.latitude) \(currentLocation.longitude)")
+    }
+    
+    public func requestWhenInUseAuthorization() {
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    public func isLocationAuthorized() -> Bool {
+        return CLLocationManager.isMonitoringAvailable(for: CLCircularRegion.self) &&
+                                        CLLocationManager.authorizationStatus() != .denied;
+    }
+    
+    //add a region with the given coordinates
+    public func doesRegionIncludeCurrentLocation() -> Bool {
+        let region = CLCircularRegion(center: coord, radius: 8046.72, identifier:  "id")        // 5 miles in meters
+        
+        return region.contains(currentLocation)
+    }
+}
--- CookieCrowdSource/CookieCrowdSource/ViewController.swift
+++ CookieCrowdSource/CookieCrowdSource/ViewController.swift
@@ -11,10 +11,22 @@ import UIKit
 class ViewController: UIViewController {
     
     let settings = SettingsViewController()
+    let locationChecker = LocationChecker()
+    var timer = Timer()
 
+    @IBOutlet weak var getCookiesButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        checkLocation()
+        
+        timer = Timer.scheduledTimer(timeInterval: 10, target: self,
+                             selector: #selector(ViewController.checkLocation), userInfo: nil, repeats: true)
     }
 
     @IBAction func tapCheckout(_ sender: Any) {
@@ -25,6 +37,47 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        timer.invalidate()
+    }
+    
+    
+    func checkLocation() {
+        if !locationChecker.isLocationAuthorized() {
+            changeToNotAuthorizedButton()
+        } else if locationChecker.doesRegionIncludeCurrentLocation() {
+            changeToGetCookiesButton()
+        } else {
+            changeToNoLocationButton()
+        }
+    }
+    
+    func changeToNotAuthorizedButton() {
+        getCookiesButton.setTitle("Allow Location in Settings", for: UIControlState())
+        getCookiesButton.isEnabled = false
+        getCookiesButton.layer.cornerRadius = 10
+    }
+    
+    func changeToNotAvailable() {
+        getCookiesButton.setTitle("Cookies Not Available", for: UIControlState())
+        getCookiesButton.isEnabled = false
+        getCookiesButton.layer.cornerRadius = 10
+    }
+    
+    func changeToGetCookiesButton() {
+        getCookiesButton.setTitle("Get Hot Cookies!", for: UIControlState())
+        getCookiesButton.isEnabled = true
+        getCookiesButton.layer.cornerRadius = 10
+    }
+    
+    func changeToNoLocationButton() {
+        getCookiesButton.setTitle("Must Be in Dayton Ohio", for: UIControlState())
+        getCookiesButton.isEnabled = false
+        getCookiesButton.layer.cornerRadius = 10
+//        getCookiesButton.layer.backgroundColor = UIColor(red:0.22, green:0.65, blue:0.91, alpha:1.00) as! CGColor
+
     }
 
 
